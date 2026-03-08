5b4731b96e8b658b3857685d9de1e5d552a878df
Shahd <158807362+shahdayman315315@users.noreply.github.com>
2026-03-08 20:59:35 +0200

    Update Program.cs

--- PowerGuard.WebApi/Program.cs
+++ PowerGuard.WebApi/Program.cs
@@ -32,11 +32,10 @@ namespace PowerGuard.WebApi
 
             }
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+           
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
